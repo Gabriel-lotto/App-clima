20d5d5d0f141d041683fbc47d0892f4c03f73e09
Gabriel-lotto <gabriel.nascimento@dcomp.ufs.br>
2021-12-16 00:31:02 -0300

    Finalizando projeto

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,73 @@
 <template>
-  <div id="app">
+  <div
+    id="app"
+    :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : '' ">
     <main>
       <div class="search-box">
-        <input 
-        type="text" 
-        class="search-bar" 
-        placeholder="Pesquise..."
-        @keypress="fetchWeater"
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Pequise alguma localidade..."
+          v-model="query"
+          @keypress="fetchWeather"
         />
       </div>
-
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">Terça 14 Dez 2021</div>
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
-      </div>
-
-      <div class="weather-box">
-        <div class="temp">9°c</div>
-        <div class="weather">Chuva</div>
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+        </div>
       </div>
     </main>
   </div>
-</template>
-
-<script>
+</template> <script>
 export default {
-  name: "App",
+  name: "app",
   data() {
     return {
       api_key: "167f1066baf6a7c24dd756c29e94ac92",
-      url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: '',
-      weather: {}
-    }
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+    };
   },
   methods: {
-    fetchWeater(e) {
-      if (e.key == "Enter") {
+    fetchWeather(e) {
+      if (e.key == "Enter") {//if pressionar enter, faz a pesquisa
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-          .then(response => {
-            return response.json();
+          .then((res) => {
+            return res.json();
           }).then(this.setResults);
       }
     },
     setResults(results) {
       this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "November", "Dezembro"];
+      let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     }
   },
 };
-</script>
-
-<style>
+</script> <style>
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 body {
-  font-family: "montserrat", sans-serif;
+  font-family: "Roboto", sans-serif;
 }
 #app {
   background-image: url("./assets/cold-bg.svg");
@@ -73,11 +81,7 @@ body {
 main {
   min-height: 100vh;
   padding: 25px;
-  background-image: linear-gradient(
-    to bottom,
-    rgba(0, 0, 0, 0.25),
-    rgba(0, 0, 0, 0.75)
-  );
+  background-image: linear-gradient(to bottom,rgba(0, 0, 0, 0.25),rgba(0, 0, 0, 0.75));
 }
 .search-box {
   width: 100%;
@@ -87,21 +91,20 @@ main {
   display: block;
   width: 100%;
   padding: 15px;
-
   color: #313131;
-  font-size: 23px;
+  font-size: 20px;
   appearance: none;
   border: none;
   outline: none;
   background: none;
-  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.5);
-  background-color: rgba(255, 255, 255, 1);
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.7);
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
 }
 .search-box .search-bar:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255, 255, 255, 0.9);
+  background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
 }
 .location-box .location {
